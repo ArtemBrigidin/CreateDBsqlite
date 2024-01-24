bde4fc9ff75c0dbefc4785e50f1f34d5ad0aa1a7
ArtemBrigidin <artemgithuub@gmail.com>
2024-01-24 11:06:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Costants.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1
+{
+    public class Costants
+    {
+        public const string DatabaseFile = "db.sqlite";
+        public const string DatabaseConnectionString = "Data Source=" + DatabaseFile;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp1;
+using Microsoft.Data.Sqlite;
+
+File.Create(Costants.DatabaseFile).Close();
+using (SqliteConnection databaseConnection = new SqliteConnection(Costants.DatabaseConnectionString))
+{
+    databaseConnection.Open();
+    SendSqlCommandNonQuery(databaseConnection, @"CREATE TABLE Name (
+                                            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            Name TEXT)");
+
+    SendSqlCommandNonQuery(databaseConnection, @"CREATE TABLE Deposit (
+                                            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            Amount TEXT)");
+
+    SendSqlCommandNonQuery(databaseConnection, @"CREATE TABLE Bondsman (
+                                            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            Name TEXT)");
+
+    void SendSqlCommandNonQuery(SqliteConnection sqliteConnection, string sqlCommand){
+        using var command = sqliteConnection.CreateCommand();
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = sqlCommand;
+        command.ExecuteNonQuery();
+    }
+}
